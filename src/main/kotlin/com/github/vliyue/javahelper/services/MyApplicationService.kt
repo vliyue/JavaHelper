78e4888f4cdf3ab6c2fb952e5e3702cb136fb80e
GitHub Action <action@github.com>
2021-04-18 13:34:40 +0000

    Template cleanup

--- src/main/kotlin/com/github/vliyue/javahelper/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vliyue/javahelper/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vliyue.javahelper.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vliyue.javahelper.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/vliyue/javahelper/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vliyue/javahelper/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vliyue.javahelper.services
 
+import com.github.vliyue.javahelper.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
